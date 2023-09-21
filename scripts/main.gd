fb2ccbd90722304e2a709923d7c1055b754616f9
Jacinto Mba Cantero <jacmba@gmail.com>
2023-09-21 14:11:02 +0100

    Retry on death

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,13 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UI/Retry.hide()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UI/Retry.visible:
+		get_tree().reload_current_scene()
+
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
 	var mob_spawn_location = $SpawnPath/SpawnLocation
@@ -15,3 +22,4 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UI/Retry.show()
